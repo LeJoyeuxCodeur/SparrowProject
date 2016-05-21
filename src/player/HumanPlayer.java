1b838d8b6ae40e1a6b8ca587b34319daffc99088
Bryan Vergauwen <bryan.vergauwen@hotmail.fr>
2016-05-21 19:13:34 +0200

    add player classes

---
+++ src/player/HumanPlayer.java
@@ -0,0 +1,15 @@
+package player;
+
+import java.util.List;
+
+import model.Boat;
+
+/**
+ * This class represents a human player, it contains all informations from a general {@link Player}
+ */
+public class HumanPlayer extends Player{
+	/**
+	 * The list of differents boats of a player
+	 */
+	private List<Boat> boat;
+}
---
+++ src/player/Player.java
@@ -0,0 +1,8 @@
+package player;
+
+/**
+ * This class represents a player, it can be a human player or a non playing player
+ */
+public abstract class Player {
+	
+}
